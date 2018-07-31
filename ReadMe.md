cb15a10916d0e4d465f8a2077f1a1a459e1b33fa
Xin Pan <1457353243@qq.com>
2018-07-31 23:31:51 +0800

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+#NsPc updates log
+Author:Xin Pan
+The program passed on Windows and edit by vs2015.
+***
+1. Add reading a .pcm file and save as a .wav file.  
+Date:2018-07-29
+2. Modify the current file reading and saving mechanism. Now you can deliver a file folder as the input and the program will read all the files in the dir and save them at dest dir.  
+Date:2018-07-30
---
+++ typedefs.h
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (c) 2011 The  project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+// This file contains platform-specific typedefs and defines.
+
+#ifndef TYPEDEFS_H_
+#define TYPEDEFS_H_
+
+// Reserved words definitions
+// TODO(andrew): Look at removing these.
+#define EXTERN extern
+#define G_CONST const
+
+
+
+    typedef signed char         Word8;
+    typedef signed short        Word16;
+    typedef signed int          Word32;
+    typedef signed long long    Word64;
+    typedef unsigned char       UWord8;
+    typedef unsigned short      UWord16;
+    typedef unsigned int        UWord32;
+    typedef unsigned long long  UWord64;
+
+
+
+
+    
+
+#endif  // TYPEDEFS_H_
